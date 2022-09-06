0566d48c1050ddeed7791d1e9e0d65a0c5a4afad
Kesims <43478239+Kesims@users.noreply.github.com>
2022-09-06 15:28:00 +0200

    update to Discord.py 2.0

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import asyncio
 import logging
 import discord
+from discord import app_commands
 from discord.ext import commands
 from discord.utils import get
-from discord_slash import SlashCommand
-from discord_slash.utils.manage_commands import create_option
 import os
 from dotenv import load_dotenv
 from paypal_api import PayPalApi
@@ -20,16 +19,14 @@ ADMIN_ID_LIST = []
 if bool(os.environ.get("ADMIN_ID_LIST") and os.environ.get("ADMIN_ID_LIST").strip()):
     ADMIN_ID_LIST = [int(i) for i in os.environ.get("ADMIN_ID_LIST").split(" ")]
 APPEAR_OFFLINE = os.getenv("APPEAR_OFFLINE").lower() == "true"
-client = discord.Client(intents=discord.Intents.all())
-bot = commands.Bot(command_prefix='!')
-slash = SlashCommand(client, sync_commands=True)
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 paypal_api = PayPalApi()
 verify_bot = VerifyBot(paypal_api)
 
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author.id == client.user.id:
+    if message.author.id == bot.user.id:
         return
     role = discord.utils.get(message.guild.roles, id=ADMIN_ROLE_ID)
     if role not in message.author.roles and message.channel.id == VERIFY_CHANNEL_ID:
@@ -37,7 +34,8 @@ async def on_message(message):
 
 
 # discord bot command to add a role to a user
-@bot.command(pass_context=True)
+#@bot.hybrid_command(pass_context=True, description="Add a role to a user")
+#@app_commands.describe(role_id="ID of the role you want to add")
 async def add_role(ctx, role_id):
     member = ctx.author
     role = get(member.guild.roles, id=int(role_id))
@@ -45,7 +43,7 @@ async def add_role(ctx, role_id):
 
 
 # send a direct message to a list of admins
-@bot.command(pass_context=True)
+#@bot.hybrid_command(pass_context=True, description="Send a direct message to a list of admins")
 async def dm_admins(ctx, email, username, roles_given, verified):
     if verified:
         message = "{} successfully verified a purchase with email: ".format(
@@ -61,9 +59,9 @@ async def dm_admins(ctx, email, username, roles_given, verified):
 
 
 # send a report message into a channel
-@bot.command(pass_context=True)
+#@bot.hybrid_command(pass_context=True, descritption="Send a report message")
 async def channel_message(author, email, username, roles, verified):
-    channel = client.get_channel(REPORT_CHANNEL_ID)
+    channel = bot.get_channel(REPORT_CHANNEL_ID)
     roles_message = ""
     if verified:
         embed = discord.Embed(title="Purchase verify of premium plugins",
@@ -81,37 +79,24 @@ async def channel_message(author, email, username, roles, verified):
         embed.add_field(name="Roles", value=roles_message, inline=False)
     await channel.send(embed=embed)
 
-
 # discord event that fires when the bot is ready and listening
-@client.event
+@bot.event
 async def on_ready():
     logging.basicConfig(handlers=[logging.FileHandler('data/verifybot.log', 'a+', 'utf-8')], level=logging.INFO,
-                        format='%(asctime)s: %(message)s')
+                       format='%(asctime)s: %(message)s')
 
     if APPEAR_OFFLINE:
-        await client.change_presence(status=discord.Status.offline)
+        await bot.change_presence(status=discord.Status.offline)
+
+    print("Syncing slash commands...")
+    await bot.tree.sync()
 
     print("The bot is ready!")
 
 
 # defines a new 'slash command' in discord and what options to show to user for params
-@slash.slash(name="verify",
-             description="Verify your plugins purchase.",
-             options=[
-                 create_option(
-                     name="email",
-                     description="Your paypal email.",
-                     option_type=3,
-                     required=True
-                 ),
-                 create_option(
-                     name="username",
-                     description="Your SpigotMc or McMarket username.",
-                     option_type=3,
-                     required=True
-                 )
-             ],
-             guild_ids=GUILD_ID)
+@bot.hybrid_command(name="verify", description="Verify your plugins purchase.")
+@app_commands.describe(email="Your PayPal email", username="Your SpigotMC or BitByBit username.")
 async def _verifypurchase(ctx, email: str, username: str):
     if not (ctx.channel.id == VERIFY_CHANNEL_ID):
         return
@@ -123,24 +108,24 @@ async def _verifypurchase(ctx, email: str, username: str):
                 await add_role(ctx, role)
                 logging.info(f"{ctx.author.name} given role: " + role)
 
-            await ctx.send(f"Successfully verified plugin purchase!", hidden=True)
+            await ctx.reply(f"Successfully verified plugin purchase!", ephemeral=True)
             await channel_message(ctx.author, email, username, roles_to_give, True)
             await dm_admins(ctx, email, username, roles_to_give, True)
             logging.info(f"{ctx.author.name} successfully verified their purchase")
             asyncio.create_task(verify_bot.write_out_emails())
     except AlreadyVerifiedPurchases:
-        await ctx.send(f"You have already verified your purchase(s)!", hidden=True)
+        await ctx.reply(f"You have already verified your purchase(s)!", ephemeral=True)
         logging.info(f"{ctx.author.name} already had all verified roles.")
         return
     except AlreadyVerifiedEmail:
-        await ctx.send(f"Purchase already verified with this email!", hidden=True)
+        await ctx.reply(f"Purchase already verified with this email!", ephemeral=True)
         logging.info(f"{ctx.author.name} already verified email.")
     except VerificationFailed:
-        await ctx.send("Failed to verify plugin purchase, open a ticket.", hidden=True)
+        await ctx.reply("Failed to verify plugin purchase, open a ticket.", ephemeral=True)
         await channel_message(ctx.author, email, username, [], False)
         await dm_admins(ctx, email, username, [], False)
         logging.info(f"{ctx.author.name} failed to verify their purchase")
 
 
 # run the discord client with the discord token
-client.run(DISCORD_TOKEN)
+bot.run(DISCORD_TOKEN)
